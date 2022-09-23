bf4f0a99389c86f857ba76ef4de51fc76e3c89e4
bita-tipi <107231389+bita-tipi@users.noreply.github.com>
2022-09-23 14:52:00 +0900

    button

--- src/routes/top/top.tsx
+++ src/routes/top/top.tsx
@@ -1,11 +1,41 @@
 import React from 'react';
 import './top.css';
 import yoshii from "../../assets/img/yoshi.png"
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 
 function Top() {
   return (
-    <div>
-      <img src={yoshii}/>
+    <div className="background">
+    <Box sx={{ '& button': { m: 1 } }}>
+      <div>
+        <Button size="small">Small</Button>
+        <Button size="medium">Medium</Button>
+        <Button size="large">Large</Button>
+      </div>
+      <div>
+        <Button variant="outlined" size="small">
+          Small
+        </Button>
+        <Button variant="outlined" size="medium">
+          Medium
+        </Button>
+        <Button variant="outlined" size="large">
+          Large
+        </Button>
+      </div>
+      <div>
+        <Button variant="contained" size="small">
+          Small
+        </Button>
+        <Button variant="contained" size="medium">
+          Medium
+        </Button>
+        <Button variant="contained" size="large">
+          Large
+        </Button>
+      </div>
+    </Box>
     </div>
   );
 }
