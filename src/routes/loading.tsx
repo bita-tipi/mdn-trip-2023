d92c3d6f3b9853197ad991cc792a9e8cb6e71d87
bita-tipi <225syunta@gmail.com>
2022-10-19 18:50:49 +0900

    tsx

---
+++ src/routes/loading.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import yoshii from "../assets/ img/yoshi.png";
+
+function Loading() {
+    return (
+        <div className="load">
+            <img src={yoshii} />
+        </div>
+    );
+}
+export default Loading;
