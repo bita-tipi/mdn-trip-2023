2f47256f190ff6d7cd3330c4e515bb7e79f76d46
shiba0329 <116796208+shiba0329@users.noreply.github.com>
2022-11-03 23:31:19 +0900

    check button 追加

--- src/routes/checklist/checklist.tsx
+++ src/routes/checklist/checklist.tsx
@@ -1,24 +1,69 @@
 import React from "react";
+import "./checklist.css";
+import Checkbox from '@mui/material/Checkbox';
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
 function Check() {
     return (
         <div>
             <p className="top">
              持ち物リスト
-            </p>
+             </p>
             
             <div className="must button">
-             <a href="#">必要なもの</a>
+             <a href="#">
+                必要なもの
+                </a>
+
+                 <ul>
+                 <Checkbox {...label} />
+                 <Checkbox {...label} />
+                 <Checkbox {...label} />
+                 <Checkbox {...label} />
+                 </ul>
+                 <ul>
+                 <Checkbox {...label} />
+                 <Checkbox {...label} />
+                 <Checkbox {...label} />
+                 <Checkbox {...label} />
+                 </ul>
             </div>
 
             <div className="benri button">
-             <a href="#">あると便利なもの</a>
+             <a href="#">
+                あると便利なもの
+             </a>
+
+                <ul>
+                 <Checkbox {...label} />
+                 <Checkbox {...label} />
+                 <Checkbox {...label} />
+                 <Checkbox {...label} />
+                 </ul>
+                 <ul>
+                 <Checkbox {...label} />
+                 <Checkbox {...label} />
+                 <Checkbox {...label} />
+                 <Checkbox {...label} />
+                 </ul>
             </div>
 
             <div className="my button">
-             <a href="#">自分だけの持ち物</a>
-            </div>
+             <a href="#">
+                自分だけの持ち物
+             </a>
 
+                <ul>
+                 <Checkbox {...label} />
+                 <Checkbox {...label} />
+                 <Checkbox {...label} />
+                 <Checkbox {...label} />
+                 </ul>
+            </div>
+            
+            <div>
+                コメント
+            </div>
 
         </div>
         
