63b57b909a83b5915748cad1a7cbd0589122cea5
shiba0329 <116796208+shiba0329@users.noreply.github.com>
2022-11-03 18:48:34 +0900

    checklist button

--- src/routes/checklist/checklist.tsx
+++ src/routes/checklist/checklist.tsx
@@ -3,8 +3,25 @@ import React from "react";
 function Check() {
     return (
         <div>
-            <p>持ち物リスト</p>
+            <p className="top">
+             持ち物リスト
+            </p>
+            
+            <div className="must button">
+             <a href="#">必要なもの</a>
+            </div>
+
+            <div className="benri button">
+             <a href="#">あると便利なもの</a>
+            </div>
+
+            <div className="my button">
+             <a href="#">自分だけの持ち物</a>
+            </div>
+
+
         </div>
+        
     );
 }
 
