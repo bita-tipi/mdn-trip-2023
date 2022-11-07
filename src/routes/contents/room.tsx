6d42e9171d287a8fcaeaf02a56400653acbc0ddd
bita-tipi <225syunta@gmail.com>
2022-11-07 16:43:50 +0900

    room HTML

--- src/routes/contents/room.tsx
+++ src/routes/contents/room.tsx
@@ -1,15 +1,17 @@
 import React from "react";
-import {PassClass,PassSuccess} from "../../assets/atom";
+import { PassClass, PassSuccess } from "../../assets/atom";
 import { useRecoilValue } from "recoil";
+import RoomHTML from "./roomHTML";
 
 function ClassRoom() {
     const currentClassData = useRecoilValue(PassClass);
     const currentSuccessData = useRecoilValue(PassSuccess);
     return (
         <div>
+            {RoomHTML()}
             <p>room</p>
             <p>{currentClassData.className}</p>
-            {currentSuccessData ?<p>success</p>:<p>error</p>}
+            {currentSuccessData ? <p>success</p> : <p>error</p>}
         </div>
     );
 }
---
+++ src/routes/contents/roomHTML.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function RoomHTML() {
+    return (
+        <body>
+            <header>
+                <p>roomHTML</p>
+            </header>
+        </body>
+    );
+}
+
+export default RoomHTML;
