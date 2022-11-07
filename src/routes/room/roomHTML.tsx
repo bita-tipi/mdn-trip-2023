e8a788bd165287bca12e81d4e78b20ed42d572ad
yuma-0624 <116692760+yuma-0624@users.noreply.github.com>
2022-11-07 18:19:10 +0900

    daimei

--- src/routes/room/roomHTML.tsx
+++ src/routes/room/roomHTML.tsx
@@ -5,8 +5,13 @@ function RoomHTML() {
     return (
         <body>
             <header>
+                <h1>ホテル名</h1>
                 <p>roomHTML</p>
             </header>
+            <main>
+                <h2>クラス名・担任</h2>
+                
+            </main>
         </body>
     );
 }
