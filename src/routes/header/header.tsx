c17989f3f10fbedf4174f7c9d5ba3921b2ab1eda
bita-tipi <225syunta@gmail.com>
2022-11-08 15:06:14 +0900

    header

--- src/routes/header/header.tsx
+++ src/routes/header/header.tsx
@@ -7,19 +7,35 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-
+import _ from "lodash";
 import "./header.css";
-import favicon from "../../assets/img/favicon.png";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 type Anchor = "top" | "left" | "bottom" | "right";
 
 function Header() {
+    function ListSelect(index: number) {
+        const copiedPage = _.cloneDeep(onPage);
+        for (let n = 0; n < 5; n++) {
+            copiedPage[n] = false;
+        }
+        copiedPage[index] = !copiedPage[index];
+        updateOnPage(copiedPage);
+    }
+
     const [state, setState] = React.useState({
         left: false,
     });
+    const [onPage, updateOnPage] = useState([
+        true,
+        false,
+        false,
+        false,
+        false,
+        false,
+    ]);
 
     const toggleDrawer =
         (anchor: Anchor, open: boolean) =>
@@ -47,14 +63,20 @@ function Header() {
             <List>
                 {
                     <div>
-                        <Link to="/">
-                            <li>HOME</li>
+                        <Link to="/" className={onPage[0] ? "yellow" : "red"}>
+                            <p onClick={() => ListSelect(0)}>HOME</p>
                         </Link>
-                        <Link to="/contents">
-                            <li>部屋割り</li>
+                        <Link
+                            to="/contents"
+                            className={onPage[1] ? "yellow" : "red"}
+                        >
+                            <p onClick={() => ListSelect(1)}>部屋割り</p>
                         </Link>
-                        <Link to="/map">
-                            <li>MAP</li>
+                        <Link
+                            to="/map"
+                            className={onPage[2] ? "yellow" : "red"}
+                        >
+                            <p onClick={() => ListSelect(2)}>MAP</p>
                         </Link>
                     </div>
                 }
@@ -63,14 +85,23 @@ function Header() {
             <List>
                 {
                     <div>
-                        <Link to="/checklist">
-                            <li>持ち物リスト</li>
+                        <Link
+                            to="/checklist"
+                            className={onPage[3] ? "yellow" : "red"}
+                        >
+                            <p onClick={() => ListSelect(3)}>持ち物リスト</p>
                         </Link>
-                        <Link to="/attention">
-                            <li>注意事項</li>
+                        <Link
+                            to="/attention"
+                            className={onPage[4] ? "yellow" : "red"}
+                        >
+                            <p onClick={() => ListSelect(4)}>注意事項</p>
                         </Link>
-                        <Link to="/schedule">
-                            <li>スケジュール</li>
+                        <Link
+                            to="/schedule"
+                            className={onPage[5] ? "yellow" : "red"}
+                        >
+                            <p onClick={() => ListSelect(5)}>スケジュール</p>
                         </Link>
                     </div>
                 }
@@ -81,22 +112,21 @@ function Header() {
     return (
         <div>
             <div>
-           {(["left"] as const).map((anchor) => (
-                <React.Fragment key={anchor}>
-                    <Button onClick={toggleDrawer(anchor, true)}>
-                        {anchor}
-                    </Button>
-                    <Drawer
-                        anchor={anchor}
-                        open={state[anchor]}
-                        onClose={toggleDrawer(anchor, false)}
-                    >
-                        {list(anchor)}
-                    </Drawer>
-                </React.Fragment>
-            ))}
+                {(["left"] as const).map((anchor) => (
+                    <React.Fragment key={anchor}>
+                        <Button onClick={toggleDrawer(anchor, true)}>
+                            {anchor}
+                        </Button>
+                        <Drawer
+                            anchor={anchor}
+                            open={state[anchor]}
+                            onClose={toggleDrawer(anchor, false)}
+                        >
+                            {list(anchor)}
+                        </Drawer>
+                    </React.Fragment>
+                ))}
             </div>
-            
         </div>
     );
 }
