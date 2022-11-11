ab88beb704e60579bb8ad6ae5db1ac91b4922d8f
naohiro21 <116626745+naohiro21@users.noreply.github.com>
2022-11-11 15:59:27 +0900

    メインを二つにわけ場所を確保。要調整の状態。デザインが変わる可能性あるため、手をつけるにはまだ早い

--- src/routes/schedule/schedule.tsx
+++ src/routes/schedule/schedule.tsx
@@ -13,8 +13,34 @@ function Schedule() {
                 <h2 className="hinichi">3日目</h2>
                 <h2 className="hinichi">4日目</h2>
             </div>
-            <div>
-            </div>
+            <main className="place">
+                <div className="timeschedule">
+                    <h3>7:20</h3>
+                    <p>7:51</p>
+                    <p></p>
+                    <p>10:01</p>
+                    <p>10:20</p>
+                    <p></p>
+                    <p>11:00</p>
+                    <p>11:20</p>
+                    <p></p>
+                    <p>11:30</p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                </div>
+                <div>
+                    <h3>名古屋駅集合</h3>
+                </div>
+            </main>
         </body>
     );
 }
