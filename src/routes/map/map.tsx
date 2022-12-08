a7856846f97949e87390a94aec000257d7b4ef46
bita-tipi <225syunta@gmail.com>
2022-12-08 16:22:15 +0900

    map

--- src/routes/map/map.tsx
+++ src/routes/map/map.tsx
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Map() {
+    const Sort_List = [Place(), Type()];
+    const [isListOpen, updateIsListOpen] = useState(false);
+    const [sort, updateSort] = useState(0);
+
+    function Sort(i: number) {
+        updateIsListOpen(false);
+        updateSort(i);
+    }
+
+    function Place() {
+        return (
+            <div>
+                <p>Place</p>
+            </div>
+        );
+    }
+    function Type() {
+        return (
+            <div>
+                <p>Type</p>
+            </div>
+        );
+    }
+
     return (
         <div>
-            <p>えぬぴーえむいんすとーる　くりえいとりあくとあっぷ　えぬぴーえむあんいんすとーるやあーん　えぬぴーえむすたーと　ぎっとこんふぃぐーーろーかるゆーざーどっとねーむだぶるくぉーてーしょんねーむだぶるくぉーてーしょん</p>
-        <p>まっちーずかんぱにー　こんふりくと　おんだんしつじゅんきこう　すたっしゅ　ぶらんちのはっこう</p>
+            <button onClick={() => updateIsListOpen(!isListOpen)}>
+                並び替え
+            </button>
+            {isListOpen ? (
+                <div>
+                    <button onClick={() => Sort(0)}>場所ごと</button>
+                    <button onClick={() => Sort(1)}>種類ごと</button>
+                </div>
+            ) : (
+                <div></div>
+            )}
+            {Sort_List[sort]}
         </div>
     );
 }
