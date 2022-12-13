21f246e147570f412b9271566ef600d73446b98b
bita-tipi <107231389+bita-tipi@users.noreply.github.com>
2022-12-13 02:47:14 +0000

    map 機能実装

--- src/routes/map/map.tsx
+++ src/routes/map/map.tsx
@@ -1,15 +1,35 @@
 import React, { useState } from "react";
+import "./map.css";
+import error from "../../assets/img/sannkau.png";
+import yoshi from "../../assets/img/yoshi.png";
+import main_logo from "../../assets/img/main_logo.svg"
 
 function Map() {
-    const Sort_List = [Place(), Type()];
-    const [isListOpen, updateIsListOpen] = useState(false);
+    const Sort_List = [Place(), Type(),History(),Culture()];
     const [sort, updateSort] = useState(0);
+    const map_List = [error,yoshi,main_logo]
+    const [map, updateMap] = useState(0);
+    const [isListOpen, updateIsListOpen] = useState(false);
 
     function Sort(i: number) {
         updateIsListOpen(false);
-        updateSort(i);
+        updateMap(i);
     }
 
+    function History() {
+        return (
+            <div>
+                <p>History</p>
+            </div>
+        );
+    }
+    function Culture() {
+        return (
+            <div>
+                <p>Culture</p>
+            </div>
+        );
+    }
     function Place() {
         return (
             <div>
@@ -27,18 +47,34 @@ function Map() {
 
     return (
         <div>
-            <button onClick={() => updateIsListOpen(!isListOpen)}>
-                並び替え
-            </button>
-            {isListOpen ? (
-                <div>
-                    <button onClick={() => Sort(0)}>場所ごと</button>
-                    <button onClick={() => Sort(1)}>種類ごと</button>
+            <div className="map-main">
+                <img className="map-map" src={map_List[map]}/>
+            </div>
+            <div className="map-TouchArea">
+                <div className="map-sort">
+                <button className="map-button" onClick={() => updateIsListOpen(!isListOpen)}>
+                    場所設定
+                </button>
                 </div>
-            ) : (
-                <div></div>
-            )}
-            {Sort_List[sort]}
+                {isListOpen ? (
+                    <div className="map-sort">
+                        <button onClick={() => Sort(0)}>広島</button>
+                        <button onClick={() => Sort(1)}>岡山</button>
+                        <button onClick={() => Sort(2)}>ユニバ</button>
+                    </div>
+                ) : (
+                    <div></div>
+                )}
+                    <div className="map-sort">
+                        <button onClick={() => updateSort(0)}>場所</button>
+                        <button onClick={() => updateSort(1)}>食べ物</button>
+                        <button onClick={() => updateSort(2)}>歴史</button>
+                        <button onClick={() => updateSort(3)}>文化</button>
+                    </div>
+                <div className="map-topic">
+                    {Sort_List[sort]}
+                </div>
+            </div>
         </div>
     );
 }
