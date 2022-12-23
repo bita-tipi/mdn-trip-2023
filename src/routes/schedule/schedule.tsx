8e7efe8fd5958e28269a09f0b2b855ff71e7dc54
bita-tipi <225syunta@gmail.com>
2022-12-23 09:52:32 +0900

    schedule

--- src/routes/schedule/schedule.tsx
+++ src/routes/schedule/schedule.tsx
@@ -2,54 +2,54 @@ import React from "react";
 import "./schedule.css";
 
 function Schedule() {
-  return (
-    <body>
-      <header>
-        <p>schedule</p>
-      </header>
-      <div className="schedule_hiduke">
-        <h2 className="schedule_hinichi">1日目</h2>
-        <h2 className="schedule_hinichi">2日目</h2>
-        <h2 className="schedule_hinichi">3日目</h2>
-        <h2 className="schedule_hinichi">4日目</h2>
-      </div>
-      <main className="schedule_place">
-        <div className="tatesen"></div>
-        <div className="timeschedule">
-          <h3>7:20</h3>
-          <p>7:51</p>
-          <p>〜6</p>
-          <p>10:01</p>
-          <p>10:20</p>
-          <p></p>
-          <h3>11:00</h3>
-          <p>11:20</p>
-          <p></p>
-          <p>11:30</p>
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
-        </div>
-        <div>
-          <h3>名古屋駅集合</h3>
-          <p>新幹線・のぞみ3号</p>
-          <h4>名古屋▶️広島</h4>
-          <h4>広島駅▶️宮島口</h4>
-          <h4>宮島口▶️宮島</h4>
-          <h4>宮島▶️宮島口</h4>
-          <h4>宮島口▶️平和祈念資料館</h4>
-        </div>
-      </main>
-    </body>
-  );
+    return (
+        <body className="schedule-body">
+            <header>
+                <p>schedule</p>
+            </header>
+            <div className="schedule_hiduke">
+                <h2 className="schedule_hinichi">1日目</h2>
+                <h2 className="schedule_hinichi">2日目</h2>
+                <h2 className="schedule_hinichi">3日目</h2>
+                <h2 className="schedule_hinichi">4日目</h2>
+            </div>
+            <main className="schedule_place">
+                <div className="tatesen"></div>
+                <div className="timeschedule">
+                    <h3>7:20</h3>
+                    <p>7:51</p>
+                    <p>〜6</p>
+                    <p>10:01</p>
+                    <p>10:20</p>
+                    <p></p>
+                    <h3>11:00</h3>
+                    <p>11:20</p>
+                    <p></p>
+                    <p>11:30</p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                </div>
+                <div>
+                    <h3>名古屋駅集合</h3>
+                    <p>新幹線・のぞみ3号</p>
+                    <h4>名古屋▶️広島</h4>
+                    <h4>広島駅▶️宮島口</h4>
+                    <h4>宮島口▶️宮島</h4>
+                    <h4>宮島▶️宮島口</h4>
+                    <h4>宮島口▶️平和祈念資料館</h4>
+                </div>
+            </main>
+        </body>
+    );
 }
 
 export default Schedule;
--- src/routes/top/top.tsx
+++ src/routes/top/top.tsx
@@ -5,7 +5,7 @@ import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
 import Loading from "../load/loading";
 import img from "../../assets/img/kari-icon.png";
-import schedule from "../../assets/img/kari-schedule.jpg"
+import schedule from "../../assets/img/kari-schedule.jpg";
 import mainvisual1 from "../../assets/img/kari-mainvisual-pc.jpg";
 import mainvisual2 from "../../assets/img/kari-mainvisual-sp.jpg";
 
@@ -19,50 +19,50 @@ function Top() {
     return (
         <div className="background_top">
             <header className="header">
-                <img src={img}/>
+                <img src={img} />
             </header>
 
             <div className="mainvisual">
-                    <img src={schedule}/>
+                <img src={schedule} />
             </div>
 
             <section className="schedule">
                 <div className="title">
-                    <img src={img}/>
+                    <img src={img} />
                     <h2>日程</h2>
                 </div>
                 <main className="schedule_main">
-                    <img src={schedule}/>
-                    <img src={schedule}/>
-                    <img src={schedule}/>
-                    <img src={schedule}/>
+                    <img src={schedule} />
+                    <img src={schedule} />
+                    <img src={schedule} />
+                    <img src={schedule} />
                 </main>
             </section>
 
             <section className="room">
                 <div className="title">
-                    <img src={img}/>
+                    <img src={img} />
                     <h2>部屋割り</h2>
                 </div>
             </section>
 
             <section className="topic">
                 <div className="title">
-                    <img src={img}/>
+                    <img src={img} />
                     <h2>トピック</h2>
                 </div>
             </section>
 
             <section className="attention">
                 <div className="title">
-                    <img src={img}/>
+                    <img src={img} />
                     <h2>注意事項</h2>
                 </div>
             </section>
 
             {isLoading ? (
                 <div className="load_top">
-                    <Loading/>
+                    <Loading />
                 </div>
             ) : (
                 <div></div>
