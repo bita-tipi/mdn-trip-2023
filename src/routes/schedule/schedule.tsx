a06b8bd13bc8d02cbd5b5f40e8c00244ad6beb30
bita-tipi <107231389+bita-tipi@users.noreply.github.com>
2022-12-25 17:40:51 +0900

    schedule

--- src/routes/schedule/schedule.tsx
+++ src/routes/schedule/schedule.tsx
@@ -1,18 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import "./schedule.css";
+import _ from "lodash";
 
 function Schedule() {
+    const [clickedList,updateClickedList] = useState([true,false,false,false])
+    const [isClicked,updateIsClicked] = useState()
+    const dayList = [Day1(),Day2(),Day3(),Day4()]
+    const [clickedDay,updateClickedDay] = useState(0);
+    
+    function Clicked(index:number){
+        updateClickedDay(index);
+        const copiedList = _.cloneDeep(clickedList);
+        for (let i = 0; i < 2; i++) {
+            copiedList[i] = false;
+        }
+        copiedList[index] = true;
+        updateClickedList(copiedList);
+    }
+
+    function Day1(){
+        return(
+            <div>
+                <p>一日目</p>
+            </div>
+        )
+    }
+
+    function Day2(){
+        return(
+            <div>
+                <p>2日目</p>
+            </div>
+        )
+    }
+
+    function Day3(){
+        return(
+            <div>
+                <p>3日目</p>
+            </div>
+        )
+    }
+
+    function Day4(){
+        return(
+            <div><p>4日目</p></div>
+        )
+    }
+
+
     return (
         <body className="schedule-body">
             <header>
                 <p>schedule</p>
             </header>
             <div className="schedule_hiduke">
-                <h2 className="schedule_hinichi">1日目</h2>
-                <h2 className="schedule_hinichi">2日目</h2>
-                <h2 className="schedule_hinichi">3日目</h2>
-                <h2 className="schedule_hinichi">4日目</h2>
+                <h2 className="schedule_hinichi" onClick={() => Clicked(0)}>1日目</h2>
+                <h2 className="schedule_hinichi" onClick={() => Clicked(1)}>2日目</h2>
+                <h2 className="schedule_hinichi" onClick={() => Clicked(2)}>3日目</h2>
+                <h2 className="schedule_hinichi" onClick={() => Clicked(3)}>4日目</h2>
             </div>
+            {dayList[clickedDay]}
             <main className="schedule_place">
                 <div className="schedule-tatesen"></div>
                 <div className="schedule-hosotatesen"></div>
