be36e72a0acce1846bc44dcb87eddaf6c8b0fe8d
Maru0214 <112690229+Maru0214@users.noreply.github.com>
2023-01-04 17:57:14 +0900

    画像入れたりいろいろしました偉いです。あけおめ！！！

--- src/routes/attention/attention.tsx
+++ src/routes/attention/attention.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import "./attention.css";
 import _ from "lodash";
-import genbakudrom from "../../assets/img/attention_genbakudorm.svg";
+import imgGembakuDome from "../../assets/img/attention_genbakudorm.svg";
+import imgTemple from "../../assets/img/attention_temple.svg";
 
 const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
@@ -16,102 +17,128 @@ function Attention() {
     }
 
     return (
-        <body >
+        <body className="attentionbody">
             <div>
                 <p>注意事項pic</p>
             </div>
             <div>
                 <ul className="tyui">
-                    <h2 onClick={() => (ListSelect(0))} className="tyuuititle">ころなについて</h2>
+                    <h2 onClick={() => (ListSelect(0))} className="tyuuititle">
+                        <div className="arrow-character">
+                            v
+                        </div>    
+                        &emsp;ころなについて
+                    </h2>
                     {isListOpen[0] ?
-                        <div className="pulldown">
-                            <li>感染者と他の同居者の部屋を可能な限り分ける</li>
-                            <li>感染者の世話をする人は、できるだけ限られた方（一人が望ましい）にする</li>
-                            <li>できるだけ全員がマスクを使用する</li>
-                            <li>小まめにうがい・手洗いをする</li>
-                            <li>いいいいいいいいい</li>
-                            <li>いいいいいいいいい</li>
+                        <div className="pulldown explanation-container">
+                            <div>
+                                <li>感染者と他の同居者の部屋を可能な限り分ける</li>
+                                <li>感染者の世話をする人は、できるだけ限られた方（一人が望ましい）にする</li>
+                                <li>できるだけ全員がマスクを使用する</li>
+                                <li>小まめにうがい・手洗いをする</li>
+                                <li>いいいいいいいいい</li>
+                                <li>いいいいいいいいい</li>
+                            </div>
+                                 <img src={imgGembakuDome} />
                         </div> : <></>}
                 </ul>
 
                 <ul className="tyui">
                     <h2 onClick={() => (ListSelect(1))} className="tyuuititle">ドコモ光ご利用時</h2>
                     {isListOpen[1] ?
-                        <div className="pulldown">
-                            <li>事前に機器を送付させていただく場合、開通工事当日の午前9時までにお客さまご自身で機器の接続をお願いいたします</li>
-                            <li>ご予約いただいた工事日を経過しても機器の接続が確認できない場合当社よりお客さまへ機器接続のお願いをご連絡することがあります。</li>
-                            <li>いいいいいいいいい</li>
-                            <li>いいいいいいいいい</li>
-                            <li>いいいいいいいいい</li>
-                            <li>いいいいいいいいい</li>
+                        <div className="pulldown explanation-container">
+                            <div>
+                                <li>感染者と他の同居者の部屋を可能な限り分ける</li>
+                                <li>感染者の世話をする人は、できるだけ限られた方（一人が望ましい）にする</li>
+                                <li>できるだけ全員がマスクを使用する</li>
+                                <li>小まめにうがい・手洗いをする</li>
+                                <li>いいいいいいいいい</li>
+                                <li>いいいいいいいいい</li>
+                            </div>
+                                 <img src={imgTemple} />
                         </div> : <></>}
+                      
                 </ul>
                 <ul className="tyui">
                     <h2 onClick={() => (ListSelect(2))} className="tyuuititle">服装について</h2>
                     {isListOpen[2] ?
-                        <div className="pulldown">
-                            <li>あああああああああ</li>
-                            <li>いいいいいいいいい</li>
-                            <li>いいいいいいいいい</li>
-                            <li>いいいいいいいいい</li>
-                            <li>いいいいいいいいい</li>
-                            <li>いいいいいいいいい</li>
+                        <div className="pulldown explanation-container">
+                            <div>
+                                <li>感染者と他の同居者の部屋を可能な限り分ける</li>
+                                <li>感染者の世話をする人は、できるだけ限られた方（一人が望ましい）にする</li>
+                                <li>できるだけ全員がマスクを使用する</li>
+                                <li>小まめにうがい・手洗いをする</li>
+                                <li>いいいいいいいいい</li>
+                                <li>いいいいいいいいい</li>
+                            </div>
+                                 <img src={imgTemple} />
                         </div> : <></>}
                 </ul>
 
                 <ul className="tyui">
                     <h2 onClick={() => (ListSelect(3))} className="tyuuititle">ヒョウドウについて</h2>
                     {isListOpen[3] ?
-                        <div className="pulldown">
-                            <li>あああああああああ</li>
-                            <li>いいいいいいいいい</li>
-                            <li>いいいいいいいいい</li>
-                            <li>いいいいいいいいい</li>
-                            <li>いいいいいいいいい</li>
-                            <li>いいいいいいいいい</li>
+                        <div className="pulldown explanation-container">
+                            <div>
+                                <li>感染者と他の同居者の部屋を可能な限り分ける</li>
+                                <li>感染者の世話をする人は、できるだけ限られた方（一人が望ましい）にする</li>
+                                <li>できるだけ全員がマスクを使用する</li>
+                                <li>小まめにうがい・手洗いをする</li>
+                                <li>いいいいいいいいい</li>
+                                <li>いいいいいいいいい</li>
+                            </div>
+                                 <img src={imgTemple} />
                         </div> : <></>}
                 </ul>
                 <ul className="tyui">
                     <h2 onClick={() => (ListSelect(4))} className="tyuuititle">ゆうまについて</h2>
                     {isListOpen[4] ?
-                        <div className="pulldown">
-                            <li>あああああああああ</li>
-                            <li>いいいいいいいいい</li>
-                            <li>いいいいいいいいい</li>
-                            <li>いいいいいいいいい</li>
-                            <li>いいいいいいいいい</li>
-                            <li>いいいいいいいいい</li>
+                        <div className="pulldown explanation-container">
+                            <div>
+                                <li>感染者と他の同居者の部屋を可能な限り分ける</li>
+                                <li>感染者の世話をする人は、できるだけ限られた方（一人が望ましい）にする</li>
+                                <li>できるだけ全員がマスクを使用する</li>
+                                <li>小まめにうがい・手洗いをする</li>
+                                <li>いいいいいいいいい</li>
+                                <li>いいいいいいいいい</li>
+                            </div>
+                                 <img src={imgTemple} />
                         </div> : <></>}
                 </ul>
                 <ul className="tyui">
                     <h2 onClick={() => (ListSelect(5))} className="tyuuititle">ながのについて</h2>
                     {isListOpen[5] ?
-                        <div className="pulldown">
-                            <li>あああああああああ</li>
-                            <li>いいいいいいいいい</li>
-                            <li>いいいいいいいいい</li>
-                            <li>いいいいいいいいい</li>
-                            <li>いいいいいいいいい</li>
-                            <li>いいいいいいいいい</li>
+                        <div className="pulldown explanation-container">
+                            <div>
+                                <li>感染者と他の同居者の部屋を可能な限り分ける</li>
+                                <li>感染者の世話をする人は、できるだけ限られた方（一人が望ましい）にする</li>
+                                <li>できるだけ全員がマスクを使用する</li>
+                                <li>小まめにうがい・手洗いをする</li>
+                                <li>いいいいいいいいい</li>
+                                <li>いいいいいいいいい</li>
+                            </div>
+                                 <img src={imgTemple} />
                         </div> : <></>}
                 </ul>
                 <ul className="tyui">
                     <h2 onClick={() => (ListSelect(6))} className="tyuuititle">しばたについて</h2>
                     {isListOpen[6] ?
-                        <div className="pulldown">
-                            <li>あああああああああ</li>
-                            <li>いいいいいいいいい</li>
-                            <li>いいいいいいいいい</li>
-                            <li>いいいいいいいいい</li>
-                            <li>いいいいいいいいい</li>
-                            <li>いいいいいいいいい</li>
+                        <div className="pulldown explanation-container">
+                            <div>
+                                <li>感染者と他の同居者の部屋を可能な限り分ける</li>
+                                <li>感染者の世話をする人は、できるだけ限られた方（一人が望ましい）にする</li>
+                                <li>できるだけ全員がマスクを使用する</li>
+                                <li>小まめにうがい・手洗いをする</li>
+                                <li>いいいいいいいいい</li>
+                                <li>いいいいいいいいい</li>
+                            </div>
+                                 <img src={imgTemple} />
                         </div> : <></>}
                 </ul>
 
             </div>
-            <div>
-                <img src={genbakudrom} />
-           </div>
+            
+           
         </body>
     );
 }
