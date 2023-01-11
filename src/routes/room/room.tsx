307362bf8838427de10e4cb91c825273bb634aa4
yuma-0624 <116692760+yuma-0624@users.noreply.github.com>
2023-01-12 00:49:48 +0900

    全然進んでいません

--- src/routes/room/room.tsx
+++ src/routes/room/room.tsx
@@ -1,19 +1,64 @@
-import React from "react";
-import { PassClass, PassSuccess } from "assets/atom";
-import { useRecoilValue } from "recoil";
-import RoomHTML from "./roomHTML";
+import React, { useState } from "react";
+import _ from "lodash";
+import "./room.css";
 
-function ClassRoom() {
-    const currentClassData = useRecoilValue(PassClass);
-    const currentSuccessData = useRecoilValue(PassSuccess);
-    return (
-        <div>
-            {RoomHTML()}
-            <p>room</p>
-            <p>{currentClassData.className}</p>
-            {currentSuccessData ? <p>success</p> : <p>error</p>}
+function RoomHTML() {
+  const [isListOpen, updateIsListOpen] = useState([
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+  ]);
+
+  function ListSelect(index: number) {
+    const copiedList = _.cloneDeep(isListOpen);
+    copiedList[index] = !copiedList[index];
+    updateIsListOpen(copiedList);
+  }
+  return (
+    <body>
+      <header>
+        <div className="hizuke">
+          <h1>一日目</h1>
+          <h1>二日目</h1>
+          <h1>三日目</h1>
         </div>
-    );
+      </header>
+      <main>
+        <section>
+          <p>ホテル</p>
+          <h3 className="hoterunonamae">〇〇ホテル</h3>
+        </section>
+        <section>
+          <p>部屋</p>
+          <div className="heyabanngou">
+            <h3 className="hoterunonamae">１１１</h3>
+            <p>号室</p>
+          </div>
+        </section>
+        <section>
+          <div className="heyabanngou">
+            <p>クラス</p>
+            <h3 className="hoterunonamae">G2D</h3>
+          </div>
+          <div className="heyabanngou">
+            <p>名前</p>
+            <h3 className="hoterunonamae">中村優馬</h3>
+          </div>
+          <div>
+            <p>メンバー</p>
+            <h3 className="mennba-nowaku">・メンバーの名前 ・メンバーの名前</h3>
+          </div>
+        </section>
+      </main>
+    </body>
+  );
 }
 
-export default ClassRoom;
+export default RoomHTML;
--- src/routes/room/roomHTML.tsx
+++ src/routes/room/roomHTML.tsx
@@ -16,226 +16,234 @@ function RoomHTML() {
         false,
     ]);
 
-    function ListSelect(index: number) {
-        const copiedList = _.cloneDeep(isListOpen);
-        copiedList[index] = !copiedList[index];
-        updateIsListOpen(copiedList);
-    }
-    return (
-        <body>
-            <header>
-                <h1>ホテル名</h1>
-                <p>roomHTML</p>
-            </header>
-            <main>
-                <h2>クラス名・担任</h2>
-                <section>
-                    <div className="heya-itibanue">
-                        <ul className="tyui">
-                            <h2 onClick={() => ListSelect(1)} className="heya">
-                                111
-                            </h2>
-                            {isListOpen[1] ? (
-                                <div className="pulldown">
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                </div>
-                            ) : (
-                                <></>
-                            )}
-                        </ul>
-                        <ul className="tyui">
-                            <h2 onClick={() => ListSelect(2)} className="heya">
-                                112
-                            </h2>
-                            {isListOpen[2] ? (
-                                <div className="pulldown">
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                </div>
-                            ) : (
-                                <></>
-                            )}
-                        </ul>
-                        <ul className="tyui">
-                            <h2 onClick={() => ListSelect(3)} className="heya">
-                                113
-                            </h2>
-                            {isListOpen[3] ? (
-                                <div className="pulldown">
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                </div>
-                            ) : (
-                                <></>
-                            )}
-                        </ul>
-                    </div>
-                    <div className="heya-itibanue">
-                        <ul className="tyui">
-                            <h2 onClick={() => ListSelect(4)} className="heya">
-                                114
-                            </h2>
-                            {isListOpen[4] ? (
-                                <div className="pulldown">
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                </div>
-                            ) : (
-                                <></>
-                            )}
-                        </ul>
-                        <ul className="tyui">
-                            <h2 onClick={() => ListSelect(5)} className="heya">
-                                115
-                            </h2>
-                            {isListOpen[5] ? (
-                                <div className="pulldown">
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                </div>
-                            ) : (
-                                <></>
-                            )}
-                        </ul>
-                        <ul className="tyui">
-                            <h2 onClick={() => ListSelect(6)} className="heya">
-                                116
-                            </h2>
-                            {isListOpen[6] ? (
-                                <div className="pulldown">
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                </div>
-                            ) : (
-                                <></>
-                            )}
-                        </ul>
-                    </div>
-                    <div className="heya-itibanue">
-                        <ul className="tyui">
-                            <h2 onClick={() => ListSelect(7)} className="heya">
-                                117
-                            </h2>
-                            {isListOpen[7] ? (
-                                <div className="pulldown">
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                </div>
-                            ) : (
-                                <></>
-                            )}
-                        </ul>
-                        <ul className="tyui">
-                            <h2 onClick={() => ListSelect(8)} className="heya">
-                                118
-                            </h2>
-                            {isListOpen[8] ? (
-                                <div className="pulldown">
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                </div>
-                            ) : (
-                                <></>
-                            )}
-                        </ul>
-                        <ul className="tyui">
-                            <h2 onClick={() => ListSelect(9)} className="heya">
-                                119
-                            </h2>
-                            {isListOpen[9] ? (
-                                <div className="pulldown">
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                    <li>名前</li>
-                                </div>
-                            ) : (
-                                <></>
-                            )}
-                        </ul>
-                    </div>
-                    <ul className="tyui">
-                        <h2 onClick={() => ListSelect(10)} className="heya">
-                            120
-                        </h2>
-                        {isListOpen[10] ? (
-                            <div className="pulldown">
-                                <li>名前</li>
-                                <li>名前</li>
-                                <li>名前</li>
-                                <li>名前</li>
-                                <li>名前</li>
-                                <li>名前</li>
-                            </div>
-                        ) : (
-                            <></>
-                        )}
-                    </ul>
-                </section>
-                <section>
-                    <h2>○月○日</h2>
-                    <p>夕食○時○分 場所〇〇</p>
-                    <p>点呼 ○時○分</p>
-                    <p>消灯 ○時○分</p>
-                </section>
-                <section>
-                    <h2>○月○日</h2>
-                    <p>起床 ○時○分</p>
-                    <p>朝食 ○時○分 場所〇〇</p>
-                    <p>点呼 ○時○分</p>
-                    <p>消灯 ○時○分</p>
-                </section>
-                <section></section>
-                <h2>○月○日</h2>
-                <p>起床 ○時○分</p>
-                <p>朝食 ○時○分 場所〇〇</p>
-                <p>点呼 ○時○分</p>
-                <p>消灯 ○時○分</p>
-                <section>
-                    <h2>○月○日</h2>
-                    <p>起床 ○時○分</p>
-                    <p>朝食 ○時○分 場所〇〇</p>
-                    <p>集合 ○時○分</p>
-                    <p>出発 ○時○分</p>
-                </section>
-            </main>
-        </body>
-    );
+  function ListSelect(index: number) {
+    const copiedList = _.cloneDeep(isListOpen);
+    copiedList[index] = !copiedList[index];
+    updateIsListOpen(copiedList)
+  }
+  return (
+    <body>
+      <header>
+      </header>
+      <main>
+        <div className="hizuke">  
+         <h1 onClick={() => ListSelect(11)} className="heya"> 1日目</h1>
+         <h1 onClick={() => ListSelect(12)} className="heya"> 2日目</h1>
+         <h1 onClick={() => ListSelect(13)} className="heya"> 3日目</h1>
+         </div>
+        <div>
+
+        <h1>ホテル名</h1>
+        <p>roomHTML</p>
+        <h2>クラス名・担任</h2>
+        <section>
+            <div className="heya-itibanue">
+              <ul className="tyui">
+                <h2 onClick={() => ListSelect(1)} className="heya">
+                 111
+                </h2>
+                {isListOpen[1] ? (
+                  <div className="pulldown">               
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>                
+                </div>
+                ) : (
+                  <></>
+                  )}
+              </ul>
+          <ul className="tyui">
+            <h2 onClick={() => ListSelect(2)} className="heya">
+              112
+            </h2>
+            {isListOpen[2] ? (
+              <div className="pulldown">
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+              </div>
+            ) : (
+              <></>
+              )}
+          </ul>
+          <ul className="tyui">
+            <h2 onClick={() => ListSelect(3)} className="heya">
+              113
+            </h2>
+            {isListOpen[3] ? (
+              <div className="pulldown">
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+              </div>
+            ) : (
+              <></>
+              )}
+          </ul>
+        </div> 
+        <div className="heya-itibanue">
+          <ul className="tyui">
+            <h2 onClick={() => ListSelect(4)} className="heya">
+              114
+            </h2>
+            {isListOpen[4] ? (
+              <div className="pulldown">
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+              </div>
+            ) : (
+              <></>
+              )}
+          </ul>
+          <ul className="tyui">
+            <h2 onClick={() => ListSelect(5)} className="heya">
+              115
+            </h2>
+            {isListOpen[5] ? (
+              <div className="pulldown">
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+              </div>
+            ) : (
+              <></>
+              )}
+          </ul>
+          <ul className="tyui">
+            <h2 onClick={() => ListSelect(6)} className="heya">
+              116
+            </h2>
+            {isListOpen[6] ? (
+              <div className="pulldown">
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+              </div>
+            ) : (
+              <></>
+              )}
+          </ul>
+          </div>
+          <div className="heya-itibanue">
+          <ul className="tyui">
+            <h2 onClick={() => ListSelect(7)} className="heya">
+              117
+            </h2>
+            {isListOpen[7] ? (
+              <div className="pulldown">
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+              </div>
+            ) : (
+              <></>
+              )}
+          </ul>
+          <ul className="tyui">
+            <h2 onClick={() => ListSelect(8)} className="heya">
+              118
+            </h2>
+            {isListOpen[8] ? (
+              <div className="pulldown">
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+              </div>
+            ) : (
+              <></>
+              )}
+          </ul>
+          <ul className="tyui">
+            <h2 onClick={() => ListSelect(9)} className="heya">
+              119
+            </h2>
+            {isListOpen[9] ? (
+              <div className="pulldown">
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+              </div>
+            ) : (
+              <></>
+              )}
+          </ul>
+          </div>
+          <ul className="tyui">
+            <h2 onClick={() => ListSelect(10)} className="heya">
+              120
+            </h2>
+            {isListOpen[10] ? (
+              <div className="pulldown">
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+                <li>名前</li>
+              </div>
+            ) : (
+              <></>
+              )}
+          </ul>
+        </section>
+        <section>
+          <h2>○月○日</h2>
+          <p>夕食○時○分 場所〇〇</p>
+          <p>点呼 ○時○分</p>
+          <p>消灯 ○時○分</p>
+        </section>
+        <section>
+          <h2>○月○日</h2>
+          <p>起床 ○時○分</p>
+          <p>朝食 ○時○分 場所〇〇</p>
+          <p>点呼 ○時○分</p>
+          <p>消灯 ○時○分</p>
+        </section>
+        <section></section>
+        <h2>○月○日</h2>
+        <p>起床 ○時○分</p>
+        <p>朝食 ○時○分 場所〇〇</p>
+        <p>点呼 ○時○分</p>
+        <p>消灯 ○時○分</p>
+        <section>
+          <h2>○月○日</h2>
+          <p>起床 ○時○分</p>
+          <p>朝食 ○時○分 場所〇〇</p>
+          <p>集合 ○時○分</p>
+          <p>出発 ○時○分</p>
+        </section>
+      </div>
+      </main>
+    </body>
+  );
 }
 
 export default RoomHTML;
