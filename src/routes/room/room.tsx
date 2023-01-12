a1b0f13dfd1bb0e1518b6980cc03bb8dd5d73d13
bita-tipi <225syunta@gmail.com>
2023-01-12 15:41:04 +0900

    room

--- src/routes/room/room.tsx
+++ src/routes/room/room.tsx
@@ -3,62 +3,147 @@ import _ from "lodash";
 import "./room.css";
 
 function RoomHTML() {
-  const [isListOpen, updateIsListOpen] = useState([
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-  ]);
+    const [isListOpen, updateIsListOpen] = useState([
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+    ]);
 
-  function ListSelect(index: number) {
-    const copiedList = _.cloneDeep(isListOpen);
-    copiedList[index] = !copiedList[index];
-    updateIsListOpen(copiedList);
-  }
-  return (
-    <body>
-      <header>
-        <div className="hizuke">
-          <h1>一日目</h1>
-          <h1>二日目</h1>
-          <h1>三日目</h1>
-        </div>
-      </header>
-      <main>
-        <section>
-          <p>ホテル</p>
-          <h3 className="hoterunonamae">〇〇ホテル</h3>
-        </section>
-        <section>
-          <p>部屋</p>
-          <div className="heyabanngou">
-            <h3 className="hoterunonamae">１１１</h3>
-            <p>号室</p>
-          </div>
-        </section>
-        <section>
-          <div className="heyabanngou">
-            <p>クラス</p>
-            <h3 className="hoterunonamae">G2D</h3>
-          </div>
-          <div className="heyabanngou">
-            <p>名前</p>
-            <h3 className="hoterunonamae">中村優馬</h3>
-          </div>
-          <div>
-            <p>メンバー</p>
-            <h3 className="mennba-nowaku">・メンバーの名前 ・メンバーの名前</h3>
-          </div>
-        </section>
-      </main>
-    </body>
-  );
+    const textList = ["１日目", "二日目", "三日目"];
+    const DayList = [Day1(), Day2(), Day3()];
+    const [clickedDay, updateClickedDay] = useState(0);
+    console.log(clickedDay);
+
+    function ListSelect(index: number) {
+        const copiedList = _.cloneDeep(isListOpen);
+        copiedList[index] = !copiedList[index];
+        updateIsListOpen(copiedList);
+    }
+
+    function Day1() {
+        return (
+            <main>
+                <section>
+                    <p>ホテル</p>
+                    <h3 className="hoterunonamae">〇〇ホテル</h3>
+                </section>
+                <section>
+                    <p>部屋</p>
+                    <div className="heyabanngou">
+                        <h3 className="hoterunonamae">222</h3>
+                        <p>号室</p>
+                    </div>
+                </section>
+                <section>
+                    <div className="heyabanngou">
+                        <p>クラス</p>
+                        <h3 className="hoterunonamae">G2D</h3>
+                    </div>
+                    <div className="heyabanngou">
+                        <p>名前</p>
+                        <h3 className="hoterunonamae">中村優馬</h3>
+                    </div>
+                    <div>
+                        <p>メンバー</p>
+                        <h3 className="mennba-nowaku">
+                            ・メンバーの名前 ・メンバーの名前
+                        </h3>
+                    </div>
+                </section>
+            </main>
+        );
+    }
+    function Day2() {
+        return (
+            <main>
+                <section>
+                    <p>ホテル</p>
+                    <h3 className="hoterunonamae">〇〇ホテル</h3>
+                </section>
+                <section>
+                    <p>部屋</p>
+                    <div className="heyabanngou">
+                        <h3 className="hoterunonamae">222</h3>
+                        <p>号室</p>
+                    </div>
+                </section>
+                <section>
+                    <div className="heyabanngou">
+                        <p>クラス</p>
+                        <h3 className="hoterunonamae">G2D</h3>
+                    </div>
+                    <div className="heyabanngou">
+                        <p>名前</p>
+                        <h3 className="hoterunonamae">中村優馬</h3>
+                    </div>
+                    <div>
+                        <p>メンバー</p>
+                        <h3 className="mennba-nowaku">
+                            ・メンバーの名前 ・メンバーの名前
+                        </h3>
+                    </div>
+                </section>
+            </main>
+        );
+    }
+    function Day3() {
+        return (
+            <main>
+                <section>
+                    <p>ホテル</p>
+                    <h3 className="hoterunonamae">〇〇ホテル</h3>
+                </section>
+                <section>
+                    <p>部屋</p>
+                    <div className="heyabanngou">
+                        <h3 className="hoterunonamae">333</h3>
+                        <p>号室</p>
+                    </div>
+                </section>
+                <section>
+                    <div className="heyabanngou">
+                        <p>クラス</p>
+                        <h3 className="hoterunonamae">G2D</h3>
+                    </div>
+                    <div className="heyabanngou">
+                        <p>名前</p>
+                        <h3 className="hoterunonamae">中村優馬</h3>
+                    </div>
+                    <div>
+                        <p>メンバー</p>
+                        <h3 className="mennba-nowaku">
+                            ・メンバーの名前 ・メンバーの名前
+                        </h3>
+                    </div>
+                </section>
+            </main>
+        );
+    }
+    return (
+        <body>
+            <header>
+                <select>
+                    <option value={0} onClick={() => updateClickedDay(0)}>
+                        １日目
+                    </option>
+                    <option value={1} onClick={() => updateClickedDay(1)}>
+                        ２日目
+                    </option>
+                    <option value={2} onClick={() => updateClickedDay(2)}>
+                        ３日目
+                    </option>
+                </select>
+            </header>
+            {DayList[clickedDay]}
+        </body>
+    );
 }
 
 export default RoomHTML;
