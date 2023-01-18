421f4ce9ae61e97a26c310daad7c57264bbddf47
ta-nu-13 <yuuuuuuuuuuka_718@au.com>
2023-01-19 00:23:10 +0900

    scheduleらへん

--- src/routes/schedule/schedule.tsx
+++ src/routes/schedule/schedule.tsx
@@ -1,55 +1,64 @@
-import React, { useState } from "react";
+import React, { ReactElement, useEffect, useState } from "react";
 import "./schedule.css";
-import _ from "lodash";
 import { ScheduleDay1 } from "./days/1";
 import { ScheduleDay2 } from "./days/2";
 import { ScheduleDay3 } from "./days/3";
 import { ScheduleDay4 } from "./days/4";
 import { assert } from "model/assets";
+import { useLocation } from "react-router-dom";
 
 export type ScheduleProps = {
-  preferDateIndex?: number;
+    preferDateIndex: number;
 };
 
-function Schedule(props: ScheduleProps) {
-  // state
-  const [dateIndex, updateDateIndex] = useState(props.preferDateIndex ?? 0);
-
-  // view
-  const components = [
+const components = [
     ScheduleDay1(),
     ScheduleDay2(),
     ScheduleDay3(),
     ScheduleDay4(),
-  ];
+];
 
-  assert((props.preferDateIndex ?? 0) > components.length, "propsが不正です");
+function Schedule() {
+    // state
+    const { state } = useLocation() as { state: ScheduleProps | undefined };
+    const preferDateIndex = state?.preferDateIndex;
+    const [dateIndex, updateDateIndex] = useState(preferDateIndex ?? 0);
 
-  function datePicker() {
-    function generatePickers() {
-      let pickers = [];
+    // local
+    useEffect(() => {
+        assert(dateIndex < components.length, "propsが不正です");
+    }, [dateIndex]);
 
-      for (let i = 0; i > components.length; i++) {
-        pickers.push(
-          <h2
-            className={`schedule_hinichi${i == dateIndex ? "-pick" : ""}`}
-            onClick={() => updateDateIndex(i)}
-          >
-            {i + 1}日目
-          </h2>
-        );
-      }
-      return pickers;
+    // view
+    function datePicker(): ReactElement {
+        function generatePickers(): ReactElement[] {
+            let pickers: ReactElement[] = [];
+
+            for (let index = 0; index < components.length; index++) {
+                pickers.push(
+                    <h2
+                        key={index}
+                        className={`schedule_hinichi${
+                            index === dateIndex ? "-pick" : ""
+                        }`}
+                        onClick={() => updateDateIndex(index)}
+                    >
+                        {index + 1}日目
+                    </h2>,
+                );
+            }
+
+            return pickers;
+        }
+        return <div className="schedule_hiduke">{generatePickers()}</div>;
     }
-    return <div className="schedule_hiduke">{generatePickers()}</div>;
-  }
 
-  return (
-    <body className="schedule-body">
-      <h1 className="schedule-title"> スケジュール</h1>
-      {datePicker()}
-      {components[dateIndex]}
-    </body>
-  );
+    return (
+        <div className="schedule-body">
+            <h1 className="schedule-title"> スケジュール</h1>
+            {datePicker()}
+            {components[dateIndex]}
+        </div>
+    );
 }
 export default Schedule;
--- src/routes/top/top.tsx
+++ src/routes/top/top.tsx
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, ReactElement } from "react";
 import "./top.css";
 import Loading from "../load/loading";
 import { getImg } from "model/assets";
-import { Link, Route, Routes } from "react-router-dom";
-import Schedule from "routes/schedule/schedule";
+import { Link, useNavigate } from "react-router-dom";
 
 const imgScheduleLogo = getImg("schedule-logo.svg");
 const imgRoomLogo = getImg("room-logo.svg");
@@ -11,91 +10,104 @@ const imgTopicLogo = getImg("topic-logo.svg");
 const imgAttentionLogo = getImg("attention-logo.svg");
 const imgSchedule = getImg("kari-schedule.jpg");
 
+const scheduleImagesPath = [imgSchedule, imgSchedule, imgSchedule, imgSchedule];
+
 function Top() {
-  useEffect(() => {
-    SetLoading(true);
-  });
+    // state
+    const [isLoading, SetLoading] = useState(false);
 
-  const [isLoading, SetLoading] = useState(false);
+    // local
+    const navigate = useNavigate();
 
-  const scheduleImgsPath = [imgSchedule, imgSchedule, imgSchedule, imgSchedule];
+    useEffect(() => {
+        SetLoading(true);
+    }, []);
 
-  function generateScheduleImgs() {
-    let scheduleImgs = [];
-    for (let i = 0; i < scheduleImgs.length; i++) {
-      scheduleImgs.push(
-        <Route path="/schedule" element={<Schedule preferDateIndex={i} />}>
-          <img src={scheduleImgsPath[i]} />
-        </Route>
-      );
+    // view
+    function generateScheduleImages() {
+        let scheduleImages: ReactElement[] = [];
+
+        for (let index = 0; index < scheduleImagesPath.length; index++) {
+            scheduleImages.push(
+                <img
+                    src={scheduleImagesPath[index]}
+                    key={index}
+                    alt={scheduleImagesPath[index]}
+                    onClick={() =>
+                        navigate("/schedule", {
+                            state: { preferDateIndex: index },
+                        })
+                    }
+                />,
+            );
+        }
+        return scheduleImages;
     }
-    return scheduleImgs;
-  }
 
-  return (
-    <div className="background_top">
-      <div className="mainvisual">
-        <img src={imgSchedule} />
-      </div>
+    return (
+        <div className="background_top">
+            <div className="mainvisual">
+                <img src={imgSchedule} />
+            </div>
 
-      <section className="schedule">
-        <div className="title">
-          <img src={imgScheduleLogo} />
-          <h2>日程</h2>
+            <section className="schedule">
+                <div className="title">
+                    <img src={imgScheduleLogo} />
+                    <h2>日程</h2>
+                </div>
+                <main className="schedule_main">
+                    <div className="border_schedule" />
+                    <div className="schedule_img">
+                        {generateScheduleImages()}
+                    </div>
+                </main>
+            </section>
+
+            <section className="room">
+                <div className="title">
+                    <img src={imgRoomLogo} />
+                    <Link to="/room">
+                        <h2>部屋割り</h2>
+                    </Link>
+                </div>
+                <div className="border_room" />
+            </section>
+
+            <section className="topic">
+                <div className="title">
+                    <img src={imgTopicLogo} />
+                    <h2>トピック</h2>
+                </div>
+                <main className="topic_main">
+                    <div className="border_topic" />
+                    <div className="topic_img">
+                        <img src={imgSchedule} />
+                        <img src={imgSchedule} />
+                        <img src={imgSchedule} />
+                        <img src={imgSchedule} />
+                    </div>
+                </main>
+            </section>
+
+            <section className="attention">
+                <div className="title">
+                    <img src={imgAttentionLogo} />
+                    <Link to="/attention">
+                        <h2>注意事項</h2>
+                    </Link>
+                </div>
+                <div className="border_attention" />
+            </section>
+
+            {isLoading ? (
+                <div className="load_top">
+                    <Loading />
+                </div>
+            ) : (
+                <div></div>
+            )}
         </div>
-        <main className="schedule_main">
-          <div className="border_schedule" />
-          <div className="schedule_img">
-            <Routes>{generateScheduleImgs()}</Routes>
-          </div>
-        </main>
-      </section>
-
-      <section className="room">
-        <div className="title">
-          <img src={imgRoomLogo} />
-          <Link to="/room">
-            <h2>部屋割り</h2>
-          </Link>
-        </div>
-        <div className="border_room" />
-      </section>
-
-      <section className="topic">
-        <div className="title">
-          <img src={imgTopicLogo} />
-          <h2>トピック</h2>
-        </div>
-        <main className="topic_main">
-          <div className="border_topic" />
-          <div className="topic_img">
-            <img src={imgSchedule} />
-            <img src={imgSchedule} />
-            <img src={imgSchedule} />
-            <img src={imgSchedule} />
-          </div>
-        </main>
-      </section>
-
-      <section className="attention">
-        <div className="title">
-          <img src={imgAttentionLogo} />
-          <Link to="/attention">
-            <h2>注意事項</h2>
-          </Link>
-        </div>
-        <div className="border_attention" />
-      </section>
-
-      {isLoading ? (
-        <div className="load_top">
-          <Loading />
-        </div>
-      ) : (
-        <div></div>
-      )}
-    </div>
-  );
+    );
 }
 
 export default Top;
