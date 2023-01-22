07a64b78f4779b37ddac84021b940e26847a41ce
naohiro21 <sotaro57@icloud.com>
2023-01-22 23:52:34 +0900

    3日目コピペ

--- src/routes/schedule/days/3.tsx
+++ src/routes/schedule/days/3.tsx
@@ -1,3 +1,41 @@
 export function ScheduleDay3() {
-  return <main></main>;
+  return (
+  <main>
+    <section>
+      <div>
+        <p id="schedule-text" className="schedule-norimono">
+            朝食
+        </p>
+      </div>
+    </section>
+    <section className="schedule_place">
+        <p id="schedule-text" className="schedule-time">
+        10:00
+        </p>
+        <div>
+        <p id="schedule-text" className="schedule-area-do">
+         観光
+        </p>
+        <p id="schedule-text" className="schedule-area-text">
+          ユニバーサルスタジオジャパン
+        </p>
+         
+        </div>
+      </section>
+      <section className="schedule_place">
+        <p id="schedule-text" className="schedule-time">
+        19:30
+        </p>
+        <div>
+          
+            <p id="schedule-text" className="schedule-area-do">
+         宿泊
+        </p>
+        <p id="schedule-text" className="schedule-area-text">
+          リーベルホテルアット<br/>ユニバーサルスタジオジャパン
+        </p>
+         
+        </div>
+      </section>
+  </main>)
 }
