e03cb2e4ac1a4910a4efa6783ca5d2765c126a8b
ta-nu-13 <yuuuuuuuuuuka_718@au.com>
2023-01-24 15:56:14 +0900

    localstrage

--- src/routes/checklist/checklist.tsx
+++ src/routes/checklist/checklist.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { KEYS, setItem, getItem, removeItem } from "assets/data/localstorage";
+import { KEYS, setItem, getItem, removeItem } from "assets/localstorage";
 import "./checklist.css";
 import Checkbox from "@mui/material/Checkbox";
 import { FormControlLabel, FormGroup } from "@mui/material";
@@ -10,205 +10,201 @@ const imgChecklist = getImg("checklist-logo.png");
 const v = getImg("v.svg");
 
 function Check() {
-    const [text, setText] = useState("");
-    const [label, setLabel] = useState("");
-    const [localStorageIndex, upDataLocalStorageIndex] = useState(0);
+  const [text, setText] = useState("");
+  const [label, setLabel] = useState("");
+  const [localStorageIndex, upDataLocalStorageIndex] = useState(0);
 
-    useEffect(() => {
-        if (localStorageIndex == 1) {
-            setClick();
-            getClick();
-        } else {
-            getClick();
-            setClick();
-        }
-        upDataLocalStorageIndex(1);
-    }, [text]);
-
-    const updateLabel = () => {
-        setLabel(getItem(KEYS.SAMPLE_TEXT));
-    };
-
-    const handleChange = (e: any) => {
-        setText(e.target.value);
-    };
-
-    const removeClick = () => {
-        // LocalStorageの値を削除する
-        removeItem(KEYS.SAMPLE_TEXT);
-        updateLabel();
-    };
-
-    const setClick = () => {
-        // テキストボックスの値をLocalStorageに保存する
-        setItem(KEYS.SAMPLE_TEXT, text);
-        updateLabel();
-    };
-
-    const getClick = () => {
-        // LocalStorageの値をテキストボックスに入れる
-        setText(getItem(KEYS.SAMPLE_TEXT));
-        updateLabel();
-    };
-
-    function ListSelect(index: number) {
-        const copiedList = _.cloneDeep(isListOpen);
-        copiedList[index] = !copiedList[index];
-        updateIsListOpen(copiedList);
+  useEffect(() => {
+    if (localStorageIndex == 1) {
+      setClick();
+      getClick();
+    } else {
+      getClick();
+      setClick();
     }
+    upDataLocalStorageIndex(1);
+  }, [text]);
 
-    const [isListOpen, updateIsListOpen] = useState([false, false, false]);
+  const updateLabel = () => {
+    setLabel(getItem(KEYS.SAMPLE_TEXT));
+  };
 
-    return (
-        <div className="checklist-body">
-            <div>
-                <h1 className="list-title">
-                    <img src={imgChecklist} className="logo" />
-                    持ち物リスト
-                </h1>
-                <div
-                    className="list-title"
-                    onClick={() => {
-                        ListSelect(0);
-                    }}
-                >
-                    必要な持ち物
-                </div>
-                {isListOpen[0] ? (
-                    <div className="my-list-pulldown">
-                        <div className="List-flexSet">
-                            <div className="List-bar"></div>
-                            <div>
-                                <div className="List-OneSet">
-                                    <Checkbox />
-                                    <p>財布</p>
-                                </div>
-                                <div className="List-OneSet">
-                                    <Checkbox />
-                                    <p>財布</p>
-                                </div>
-                                <div className="List-OneSet">
-                                    <Checkbox />
-                                    <p>財布</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="List-flexSet">
-                            <div className="List-bar"></div>
-                            <div>
-                                <div className="List-OneSet">
-                                    <Checkbox />
-                                    <p>財布</p>
-                                </div>
-                                <div className="List-OneSet">
-                                    <Checkbox />
-                                    <p>財布</p>
-                                </div>
-                                <div className="List-OneSet">
-                                    <Checkbox />
-                                    <p>財布</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="List-flexSet">
-                            <div className="List-bar"></div>
-                            <div>
-                                <div className="List-OneSet">
-                                    <Checkbox />
-                                    <p>財布</p>
-                                </div>
-                                <div className="List-OneSet">
-                                    <Checkbox />
-                                    <p>財布</p>
-                                </div>
-                                <div className="List-OneSet">
-                                    <Checkbox />
-                                    <p>財布</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                ) : (
-                    <div className="List-closeAnimation">
-                        <div className="My-List-pulldown">
-                            <div className="List-flexSet">
-                                <div className="List-bar"></div>
-                                <div>
-                                    <div className="List-OneSet">
-                                        <Checkbox />
-                                        <p>財布</p>
-                                    </div>
-                                    <div className="List-OneSet">
-                                        <Checkbox />
-                                        <p>財布</p>
-                                    </div>
-                                    <div className="List-OneSet">
-                                        <Checkbox />
-                                        <p>財布</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="List-flexSet">
-                                <div className="List-bar"></div>
-                                <div>
-                                    <div className="List-OneSet">
-                                        <Checkbox />
-                                        <p>財布</p>
-                                    </div>
-                                    <div className="List-OneSet">
-                                        <Checkbox />
-                                        <p>財布</p>
-                                    </div>
-                                    <div className="List-OneSet">
-                                        <Checkbox />
-                                        <p>財布</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="List-flexSet">
-                                <div className="List-bar"></div>
-                                <div>
-                                    <div className="List-OneSet">
-                                        <Checkbox />
-                                        <p>財布</p>
-                                    </div>
-                                    <div className="List-OneSet">
-                                        <Checkbox />
-                                        <p>財布</p>
-                                    </div>
-                                    <div className="List-OneSet">
-                                        <Checkbox />
-                                        <p>財布</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                )}
-                <div
-                    className="list-title"
-                    onClick={() => {
-                        ListSelect(2);
-                    }}
-                >
-                    自分だけの持ち物
-                </div>
-                {isListOpen[2] ? (
-                    <div className="my-list-pulldown">
-                        <ul>
-                            <Checkbox />
-                            <input
-                                type="text"
-                                value={text}
-                                onChange={handleChange}
-                            />
-                        </ul>
-                    </div>
-                ) : (
-                    <div></div>
-                )}
-            </div>
+  const handleChange = (e: any) => {
+    setText(e.target.value);
+  };
+
+  const removeClick = () => {
+    // LocalStorageの値を削除する
+    removeItem(KEYS.SAMPLE_TEXT);
+    updateLabel();
+  };
+
+  const setClick = () => {
+    // テキストボックスの値をLocalStorageに保存する
+    setItem(KEYS.SAMPLE_TEXT, text);
+    updateLabel();
+  };
+
+  const getClick = () => {
+    // LocalStorageの値をテキストボックスに入れる
+    setText(getItem(KEYS.SAMPLE_TEXT));
+    updateLabel();
+  };
+
+  function ListSelect(index: number) {
+    const copiedList = _.cloneDeep(isListOpen);
+    copiedList[index] = !copiedList[index];
+    updateIsListOpen(copiedList);
+  }
+
+  const [isListOpen, updateIsListOpen] = useState([false, false, false]);
+
+  return (
+    <div className="checklist-body">
+      <div>
+        <h1 className="list-title">
+          <img src={imgChecklist} className="logo" />
+          持ち物リスト
+        </h1>
+        <div
+          className="list-title"
+          onClick={() => {
+            ListSelect(0);
+          }}
+        >
+          必要な持ち物
         </div>
-    );
+        {isListOpen[0] ? (
+          <div className="my-list-pulldown">
+            <div className="List-flexSet">
+              <div className="List-bar"></div>
+              <div>
+                <div className="List-OneSet">
+                  <Checkbox />
+                  <p>財布</p>
+                </div>
+                <div className="List-OneSet">
+                  <Checkbox />
+                  <p>財布</p>
+                </div>
+                <div className="List-OneSet">
+                  <Checkbox />
+                  <p>財布</p>
+                </div>
+              </div>
+            </div>
+            <div className="List-flexSet">
+              <div className="List-bar"></div>
+              <div>
+                <div className="List-OneSet">
+                  <Checkbox />
+                  <p>財布</p>
+                </div>
+                <div className="List-OneSet">
+                  <Checkbox />
+                  <p>財布</p>
+                </div>
+                <div className="List-OneSet">
+                  <Checkbox />
+                  <p>財布</p>
+                </div>
+              </div>
+            </div>
+            <div className="List-flexSet">
+              <div className="List-bar"></div>
+              <div>
+                <div className="List-OneSet">
+                  <Checkbox />
+                  <p>財布</p>
+                </div>
+                <div className="List-OneSet">
+                  <Checkbox />
+                  <p>財布</p>
+                </div>
+                <div className="List-OneSet">
+                  <Checkbox />
+                  <p>財布</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className="List-closeAnimation">
+            <div className="My-List-pulldown">
+              <div className="List-flexSet">
+                <div className="List-bar"></div>
+                <div>
+                  <div className="List-OneSet">
+                    <Checkbox />
+                    <p>財布</p>
+                  </div>
+                  <div className="List-OneSet">
+                    <Checkbox />
+                    <p>財布</p>
+                  </div>
+                  <div className="List-OneSet">
+                    <Checkbox />
+                    <p>財布</p>
+                  </div>
+                </div>
+              </div>
+              <div className="List-flexSet">
+                <div className="List-bar"></div>
+                <div>
+                  <div className="List-OneSet">
+                    <Checkbox />
+                    <p>財布</p>
+                  </div>
+                  <div className="List-OneSet">
+                    <Checkbox />
+                    <p>財布</p>
+                  </div>
+                  <div className="List-OneSet">
+                    <Checkbox />
+                    <p>財布</p>
+                  </div>
+                </div>
+              </div>
+              <div className="List-flexSet">
+                <div className="List-bar"></div>
+                <div>
+                  <div className="List-OneSet">
+                    <Checkbox />
+                    <p>財布</p>
+                  </div>
+                  <div className="List-OneSet">
+                    <Checkbox />
+                    <p>財布</p>
+                  </div>
+                  <div className="List-OneSet">
+                    <Checkbox />
+                    <p>財布</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+        <div
+          className="list-title"
+          onClick={() => {
+            ListSelect(2);
+          }}
+        >
+          自分だけの持ち物
+        </div>
+        {isListOpen[2] ? (
+          <div className="my-list-pulldown">
+            <ul>
+              <Checkbox />
+              <input type="text" value={text} onChange={handleChange} />
+            </ul>
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </div>
+    </div>
+  );
 }
 export default Check;
