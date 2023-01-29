5cbb0f941db7cde2fc78d75e4386b9ed381b98a1
bita-tipi <107231389+bita-tipi@users.noreply.github.com>
2023-01-29 18:57:07 +0900

    こみっとわすれ

--- src/routes/map/map.tsx
+++ src/routes/map/map.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./map.css";
 import { getImg } from "model/assets";
 
-const imgMapAichi = getImg("地図あいち.svg");
+const imgMapKiyomizu = getImg("清水寺.png");
 const imgYoshi = getImg("yoshi.png");
 const imgMainLogo = getImg("main_logo.svg");
 const imgFoods = getImg("食べ物.png");
@@ -11,18 +11,44 @@ const imgHistory = getImg("歴史.png");
 const imgGift = getImg("おみやげ.png");
 const imgBorder = getImg("境界.png");
 const imgMap = getImg("地図.png");
-const imgHistoryKurashiki = getImg("倉敷歴史.png");
+
+const imgHistoryKurashiki = getImg("topic/倉敷歴史.png");
+const yuniba1 = getImg("topic/yuniba1.png");
+const yuniba2 = getImg("topic/ゆにばアトラクション2.png");
+const yuniba3 = getImg("topic/ゆにばアトラクション3.png");
+const yuniba4 = getImg("topic/ゆにばお土産.png");
+const yuniba5 = getImg("topic/ゆにばお土産2.png");
+const yuniba6 = getImg("topic/ゆにばお土産3.png");
+const yuniba7 = getImg("topic/ゆにばお土産4.png");
+const kyotoArea = getImg("topic/京都場所.png");
+const kyotoHitory = getImg("topic/京都歴史.png");
+const hiroshima1 = getImg("topic/広島お土産.png");
+const hiroshima2 = getImg("topic/広島お土産2.png");
+const hiroshimaFood = getImg("topic/広島食べ物1.png");
+const hiroshimaFood2 = getImg("topic/広島食べ物2.png");
+const hiroshimaHistory = getImg("topic/広島歴史.png");
+const kurashikiPlace1 = getImg("topic/倉敷場所.png");
+const kurashiloPlace2 = getImg("topic/倉敷場所2.png");
+const kurashiloPlace3 = getImg("topic/倉敷場所3.png");
+const kurashiloFood1 = getImg("topic/倉敷食べ物2.png");
+const kurashiloFood2 = getImg("topic/倉敷食べ物3.png");
+const kurashiloHistory1 = getImg("topic/倉敷歴史.png");
+const kurashiloHistory2 = getImg("topic/倉敷歴史2.png");
+const miyajima1 = getImg("topic/宮島食べ物.png");
+const miyajima2 = getImg("topic/宮島食べ物2.png");
+
+
 const imgTopicArea = getImg("topic-area.png");
 
 function Map() {
     const Sort_List = [Place(), Food(), Gift(), History()];
     const [sort, updateSort] = useState(0);
-    const map_List = [imgMapAichi, imgYoshi, imgMainLogo];
+    const map_List = [imgMapKiyomizu, imgYoshi, imgMainLogo,imgMainLogo,imgMainLogo,imgMainLogo,imgMainLogo];
     const [map, updateMap] = useState(0);
     const [isListOpen, updateIsListOpen] = useState(false);
     const [topicNumber, upDateTopicNumber] = useState(0);
-    const topicList = [imgTopicArea, imgHistoryKurashiki];
-    const areaList = ["広島", "倉敷", "ユニバ", "嵐山", "清水坂", "北野天満宮"];
+    const topicList = [imgTopicArea,yuniba1,yuniba4,kyotoArea,kyotoHitory,hiroshima1,hiroshimaFood,hiroshimaHistory,kurashikiPlace1,kurashiloFood1,kurashiloHistory1,miyajima1];
+    const areaList = ["広島", "倉敷", "ユニバ", "清水寺", "北野天満宮", "嵐山"];
     const [areaNumber, upDataNumber] = useState(0);
 
     function Sort(i: number) {
@@ -40,6 +66,26 @@ function Map() {
                         <img src={imgPlace} className="map-topic-icon" />
                         <p className="map-topic-text">場所</p>
                     </div>
+                    <div className="topicButtonAreaSelect">
+                        <button
+                            onClick={() => upDateTopicNumber(8)}
+                            className="map-selectButton"
+                        >
+                            倉敷
+                        </button>
+                        <button
+                            onClick={() => upDateTopicNumber(1)}
+                            className="map-selectButton"
+                        >
+                            ユニバ
+                        </button>
+                        <button
+                            onClick={() => upDateTopicNumber(3)}
+                            className="map-selectButton"
+                        >
+                            京都
+                        </button>
+                    </div>
                 </div>
             </div>
         );
@@ -52,6 +98,32 @@ function Map() {
                     <div className="map-topic-in">
                         <img src={imgFoods} className="map-topic-icon" />
                         <p className="map-topic-text">食べ物</p>
+                    </div>
+                    <div className="topicButtonAreaSelect">
+                        <button
+                            onClick={() => upDateTopicNumber(9)}
+                            className="map-selectButton"
+                        >
+                            倉敷
+                        </button>
+                        <button
+                            onClick={() => upDateTopicNumber(6)}
+                            className="map-selectButton"
+                        >
+                            広島
+                        </button>
+                        <button
+                            onClick={() => upDateTopicNumber(1)}
+                            className="map-selectButton"
+                        >
+                            京都
+                        </button>
+                        <button
+                            onClick={() => upDateTopicNumber(2)}
+                            className="map-selectButton"
+                        >
+                            ユニバ
+                        </button>
                     </div>
                 </div>
             </div>
@@ -66,6 +138,26 @@ function Map() {
                         <img src={imgGift} className="map-topic-icon" />
                         <p className="map-topic-text">お土産</p>
                     </div>
+                    <div className="topicButtonAreaSelect">
+                        <button
+                            onClick={() => upDateTopicNumber(5)}
+                            className="map-selectButton"
+                        >
+                            広島
+                        </button>
+                        <button
+                            onClick={() => upDateTopicNumber(11)}
+                            className="map-selectButton"
+                        >
+                            宮島
+                        </button>
+                        <button
+                            onClick={() => upDateTopicNumber(2)}
+                            className="map-selectButton"
+                        >
+                            ユニバ
+                        </button>
+                    </div>
                 </div>
             </div>
         );
@@ -79,12 +171,24 @@ function Map() {
                         <img src={imgHistory} className="map-topic-icon" />
                         <p className="map-topic-text">歴史</p>
                     </div>
-                    <div>
+                    <div className="topicButtonAreaSelect">
                         <button
-                            onClick={() => upDateTopicNumber(1)}
+                            onClick={() => upDateTopicNumber(10)}
                             className="map-selectButton"
                         >
                             倉敷
+                        </button>
+                        <button
+                            onClick={() => upDateTopicNumber(7)}
+                            className="map-selectButton"
+                        >
+                            広島
+                        </button>
+                        <button
+                            onClick={() => upDateTopicNumber(4)}
+                            className="map-selectButton"
+                        >
+                            京都
                         </button>
                     </div>
                 </div>
@@ -110,6 +214,12 @@ function Map() {
                                 <div className="map-red-circle"></div>
                                 <div className="map-red-bar"></div>
                                 <div className="map-red-circle"></div>
+                                <div className="map-red-bar"></div>
+                                <div className="map-red-circle"></div>
+                                <div className="map-red-bar"></div>
+                                <div className="map-red-circle"></div>
+                                <div className="map-red-bar"></div>
+                                <div className="map-red-circle"></div>
                             </div>
                             <div className="map-bar-open"></div>
                             <div className="map-pull-list">
@@ -129,6 +239,15 @@ function Map() {
                                 </p>
                                 <p onClick={() => Sort(2)} className="map-pull">
                                     ユニバ
+                                </p>
+                                <p onClick={() => Sort(3)} className="map-pull">
+                                    清水寺
+                                </p>
+                                <p onClick={() => Sort(4)} className="map-pull">
+                                    北野天満宮
+                                </p>
+                                <p onClick={() => Sort(5)} className="map-pull">
+                                    嵐山
                                 </p>
                             </div>
                         </div>
