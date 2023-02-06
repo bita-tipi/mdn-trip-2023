cb0e3e9a44b39274b202d5d93f916498e017f26b
bita-tipi <107231389+bita-tipi@users.noreply.github.com>
2023-02-06 04:35:50 +0000

    hmm

--- src/routes/map/map.tsx
+++ src/routes/map/map.tsx
@@ -204,7 +204,7 @@ function Map() {
     return (
         <div className="map-body">
             <div className="map-main">
-                <PanZoom height={100} width={100} >
+                <PanZoom>
                     <img className="map-map" src={map_List[map]}></img>
                 </PanZoom>
             </div>
